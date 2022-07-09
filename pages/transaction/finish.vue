d1f6ea7a3db3e881fb288fe0e5de47984224b8b0
icat108 <ichacnakval@gmail.com>
2022-07-09 19:14:29 +0700

    this

---
+++ pages/transaction/finish.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container mx-auto h-screen flex justify-center items-center">
+    <div class="w-full lg:w-1/3 px-10 lg:px-0">
+      <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+        <img src="/funded-illustration.svg" alt="" class="w-full" />
+      </div>
+      <h2 class="font-medium mb-3 text-3xl text-center">Ya! Kamu Super</h2>
+      <p class="text-center font-light">
+       Uang Anda telah ditransfer
+        <br />
+        ke rekening perusahaan
+      </p>
+      <div class="mb-4 mt-6">
+        <div class="mb-3">
+          <button
+            @click="$router.push({ path: '/' })"
+            class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
+          >
+            Mendanai Proyek Lainnya
+          </button>
+        </div>
+        <div class="mb-4">
+          <button
+            @click="$router.push({ path: '/dashboard' })"
+            class="block w-full bg-transparent border border-gray-500 text-gray-500 hover:bg-green-button hover:border-green-button hover:text-white font-light px-6 py-4 text-lg rounded-full"
+          >
+            Kembali ke dasboard
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'success',
+}
+</script>
+
+<style lang="scss" scoped>
+.auth-background {
+  background-image: url('/sign-in-background.jpg');
+  background-position: center;
+  background-size: cover;
+}
+</style>
